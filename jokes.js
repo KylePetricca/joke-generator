76a86f9ed57188801cbfb96d526bfacf100de840
KylePetricca <129595082+KylePetricca@users.noreply.github.com>
2023-04-05 17:24:45 +0100

    Add files via upload

---
+++ jokes.js
@@ -0,0 +1,20 @@
+// jokes.js
+
+export const jokes = [
+    {
+      category: "Programming",
+      setup: "Why do programmers prefer dark mode?",
+      punchline: "Because light attracts bugs."
+    },
+    {
+      category: "Knock-knock",
+      setup: "Knock, knock. \n Who’s there? \n Boo. \n Boo who?",
+      punchline: "Don’t cry, it’s just a joke."
+    },
+    {
+      category: "Dad Jokes",
+      setup: "Why don't scientists trust atoms?",
+      punchline: "Because they make up everything."
+    }
+  ];
+  
--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-const jokeText = document.getElementById("jokeText");
-const generateBtn = document.getElementById("generateBtn");
+// Define an array of jokes, each with a category, setup, and punchline
+// main.js
 
-generateBtn.addEventListener("click", getJoke);
+import { jokes } from "./jokes.js";
 
-async function getJoke() {
-    const apiUrl = "https://official-joke-api.appspot.com/random_joke";
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        jokeText.innerHTML = `${data.setup} <br> ${data.punchline}`;
-    } catch (error) {
-        jokeText.innerHTML = "Oops, something went wrong. Please try again later.";
-    }
-}
+// The rest of your JavaScript code that uses the `jokes` array
+
+  
+  // Get references to the button, paragraph, and form elements
+  const generateBtn = document.getElementById("generateBtn");
+  const jokeText = document.getElementById("jokeText");
+  const addJokeForm = document.getElementById("addJokeForm");
+  
+  // Add an event listener to the button that generates a random joke
+  generateBtn.addEventListener("click", () => {
+    // Choose a random joke from the array
+    const randomIndex = Math.floor(Math.random() * jokes.length);
+    const randomJoke = jokes[randomIndex];
+    
+    // Build the joke string
+    const jokeString = `${randomJoke.category} Joke:\n${randomJoke.setup}\n${randomJoke.punchline}`;
+  
+    // Display the joke in the paragraph element
+    jokeText.textContent = jokeString;
+  });
+  
+  // Add an event listener to the form that adds a new joke to the array
+  addJokeForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    // Get the input values from the form
+    const categoryInput = document.getElementById("categoryInput").value;
+    const setupInput = document.getElementById("setupInput").value;
+    const punchlineInput = document.getElementById("punchlineInput").value;
+    
+    // Create a new joke object from the input values
+    const newJoke = {
+      category: categoryInput,
+      setup: setupInput,
+      punchline: punchlineInput
+    };
+    
+    // Add the new joke object to the array
+    jokes.push(newJoke);
+    
+    // Reset the form inputs
+    addJokeForm.reset();
+  });
